ac67d8461d639fb624acfd2b0272a51b5e515dd2
ReZeY007 <rozhkov.andr2009@gmail.com>
2026-06-30 00:33:38 +0300

    Made an UI implementation of adding and removing tasks from a new list

--- frontend/src/pages/NewList.jsx
+++ frontend/src/pages/NewList.jsx
@@ -3,17 +3,85 @@ import { useLoaderData } from "react-router";
 import "./NewList.css";
 
 function NewList() {
-  const [addedTasks, setAddedTasks] = useState([]);
   const loaderData = useLoaderData();
-  const availableTasks = loaderData?.availableTasks;
-  console.log(availableTasks);
+  const [addedTasks, setAddedTasks] = useState([]);
+  const [availableTasks, setAvailableTasks] = useState(
+    loaderData?.availableTasks,
+  );
+
+  const handleAddTaskClick = (e) => {
+    const target = e.target;
+    const availableTaskElem = target.closest(".available-task");
+    const availableTask = availableTasks.find(
+      (availTask) => availTask.id == availableTaskElem.dataset.taskid,
+    );
+
+    const newAvailableTasksList = availableTasks.filter((availTask) => {
+      if (availTask.id == availableTaskElem.dataset.taskid) return false;
+      return true;
+    });
+
+    setAddedTasks([...addedTasks, availableTask]);
+    setAvailableTasks(newAvailableTasksList);
+  };
+
+  const handleRemoveTaskClick = (e) => {
+    const target = e.target;
+    const addedTaskElem = target.closest(".added-task");
+    const addedTask = addedTasks.find(
+      (addedTask) => addedTask.id == addedTaskElem.dataset.taskid,
+    );
+
+    const newAddedTasksList = addedTasks.filter((addedTask) => {
+      if (addedTask.id == addedTaskElem.dataset.taskid) return false;
+      return true;
+    });
+
+    setAvailableTasks([...availableTasks, addedTask]);
+    setAddedTasks(newAddedTasksList);
+  };
 
   return (
     <div className="block new-list-page">
-      <input className="list-title" placeholder="Title" required />
-      {addedTasks && <section className="added-tasks"></section>}
+      <div className="new-list__header">
+        <input className="list-title" placeholder="Title" required />
+        <button>Create</button>
+      </div>
+      {addedTasks.length ? (
+        <section className="added-tasks">
+          <h2>Added tasks</h2>
+          <ul className="added-tasks">
+            {addedTasks.map((addedTask) => (
+              <div
+                className="added-task"
+                style={{ border: "solid 2px white" }}
+                data-taskid={addedTask.id}
+                key={addedTask.id}
+              >
+                <h3>{addedTask.title}</h3>
+                <p>{addedTask.description}</p>
+                <button onClick={handleRemoveTaskClick}>Remove</button>
+              </div>
+            ))}
+          </ul>
+        </section>
+      ) : null}
       <section className="available-tasks">
         <h2>Available tasks</h2>
+        <ul className="available-tasks">
+          {availableTasks.map((availTask) => (
+            <div
+              className="available-task"
+              style={{ border: "solid 2px white" }}
+              data-taskid={availTask.id}
+              key={availTask.id}
+            >
+              <h3>{availTask.title}</h3>
+              <p>{availTask.description}</p>
+              <button onClick={handleAddTaskClick}>Add</button>
+            </div>
+          ))}
+        </ul>
       </section>
     </div>
   );
